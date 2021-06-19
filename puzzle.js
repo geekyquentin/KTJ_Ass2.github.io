1ad8656ed0329adfd88dc1e9c952114330cec47a
Yashwant Krishna Pagoti <pagotiyashwantkrishna@gmail.com>
2021-06-19 07:45:50 +0530

    can't add class to table elements

--- puzzle.js
+++ puzzle.js
@@ -1,6 +1,7 @@
 var flag = 0;
 var youWin = 0;
 var Size = 4;
+var usercount = 0;
 selectlayout(4);
 
 function pauseClick() {
@@ -141,6 +142,19 @@ function selectlayout(Size) {
 			var newStr = str.substring(0, str.length - 1);
 			document.getElementById("final-moves-value").innerHTML = newStr;
 			sw.stop();
+			var table = document.getElementById("myTable");
+
+			var row = table.insertRow(usercount + 1);
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
+			var cell4 = row.insertCell(3);
+
+			cell1.innerHTML = usercount;
+			cell2.innerHTML = "Yashwant Krishna";
+			cell3.innerHTML = newStr;
+			cell4.innerHTML = count;
+			usercount++;
 		} else {
 			console.log("Not yet win vro");
 		}
